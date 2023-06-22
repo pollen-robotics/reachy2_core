3943f837567bb4de820ef980f1a42e1ccea2cadf
Pierre Rouanet <pierre.rouanet@gmail.com>
2023-06-22 14:25:24 +0200

    Add system hwi for orbita2d.

---
+++ reachy_controllers/orbita2d_serial_system_hwi/include/orbita2d_serial_system_hwi/orbita2d_serial_system_hwi.hpp
@@ -0,0 +1,62 @@
+#ifndef _ORBITA2D_SERIAL_SYSTEM_HWI
+#define _ORBITA2D_SERIAL_SYSTEM_HWI
+
+#include "rclcpp/macros.hpp"
+
+#include "rclcpp/clock.hpp"
+
+#include "hardware_interface/system_interface.hpp"
+#include "hardware_interface/types/hardware_interface_type_values.hpp"
+
+#define LOG_THROTTLE_DURATION 30000
+
+namespace orbita2d_serial_system_hwi
+{
+  using namespace hardware_interface;
+class Orbita2dSerialSystem : public hardware_interface::SystemInterface
+{
+public:
+    RCLCPP_SHARED_PTR_DEFINITIONS(Orbita2dSerialSystem)
+
+    CallbackReturn on_init(const hardware_interface::HardwareInfo & info) override;
+
+    std::vector<hardware_interface::StateInterface> export_state_interfaces() override;
+    std::vector<hardware_interface::CommandInterface> export_command_interfaces() override;
+
+    CallbackReturn on_activate(const rclcpp_lifecycle::State & previous_state) override;
+    CallbackReturn on_deactivate(const rclcpp_lifecycle::State & previous_state) override;
+
+    hardware_interface::return_type read(const rclcpp::Time &, const rclcpp::Duration &) override;
+    hardware_interface::return_type write(const rclcpp::Time &, const rclcpp::Duration &) override;
+
+private:
+  double hw_states_position_[2];
+  double hw_states_velocity_[2];
+  double hw_states_effort_[2];
+  double hw_states_temperature_[2];
+  double hw_states_torque_limit_[2];
+  double hw_states_speed_limit_[2];
+  double hw_states_torque_[2];
+  double hw_states_p_gain_[2];
+  double hw_states_i_gain_[2];
+  double hw_states_d_gain_[2];
+
+  double hw_commands_position_[2];
+  double hw_commands_speed_limit_[2];
+  double hw_commands_torque_limit_[2];
+  double hw_commands_torque_[2];
+  double hw_commands_p_gain_[2];
+  double hw_commands_i_gain_[2];
+  double hw_commands_d_gain_[2];
+
+  // Store time between update loops
+  rclcpp::Clock clock_;
+  rclcpp::Time last_timestamp_;
+  rclcpp::Time current_timestamp;  // Avoid initialization on each read
+
+  uint32_t uid;
+};
+
+}
+
+#endif // _ORBITA2D_SERIAL_SYSTEM_HWI
