6ddf42244833b69b5705843d29aaa739d349e3f7
Pierre Rouanet <pierre.rouanet@gmail.com>
2023-06-22 14:25:03 +0200

    Rename 2dof to 2d.

--- reachy_controllers/reachy_2023_controllers_rs/orbita2d_serial_hwi/build.rs
+++ reachy_controllers/reachy_2023_controllers_rs/orbita2d_serial_hwi/build.rs
@@ -9,5 +9,5 @@ fn main() {
         .with_crate(crate_dir)
         .generate()
         .expect("Unable to generate bindings")
-        .write_to_file("orbita2dof_serial_hwi.h");
+        .write_to_file("orbita2d_serial_hwi.h");
 }
