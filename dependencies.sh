6b4ed336a4685f3b9b9d1a4a9dd4e0af93df7e15
Clément Dussieux <clement.dussieux@pollen-robotics.com>
2023-09-08 14:29:53 +0200

    update dependencies and requirements

--- dependencies.sh
+++ dependencies.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-sudo apt-get install -y python3-pip
+sudo apt-get install -y python3-pip python3-pykdl
 sudo apt-get install -y ament-cmake
-sudo apt-get install -y python3-pykdl
 sudo apt-get install -y ros-humble-ros2-control ros-humble-joint-state-publisher-gui ros-humble-ros2-controllers ros-humble-xacro ros-humble-rmw-cyclonedds-cpp ros-humble-gazebo-ros ros-humble-gazebo-ros-pkgs ros-humble-gazebo-ros2-control  ros-humble-tf-transformations
-sudo apt-get install -y libclang-dev
+sudo apt-get install -y libclang-dev libudev-dev pkg-config
 #cargo install cargo-ament-build
 cargo install --git https://github.com/jerry73204/cargo-ament-build.git --branch conditionally-copy-cargo-lock-file
