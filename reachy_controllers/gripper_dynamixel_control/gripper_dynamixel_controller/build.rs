4c633a8f60fe3b8d2a8e91f26a671ae4d222ba90
Pierre Rouanet <pierre.rouanet@gmail.com>
2024-01-04 15:21:41 +0100

    Remove smart from filenames.

--- reachy_controllers/gripper_dynamixel_control/gripper_dynamixel_controller/build.rs
+++ reachy_controllers/gripper_dynamixel_control/gripper_dynamixel_controller/build.rs
@@ -9,5 +9,5 @@ fn main() {
         .with_crate(crate_dir)
         .generate()
         .expect("Unable to generate bindings")
-        .write_to_file("smart_gripper_dynamixel_hwi.h");
+        .write_to_file("gripper_dynamixel_hwi.h");
 }
