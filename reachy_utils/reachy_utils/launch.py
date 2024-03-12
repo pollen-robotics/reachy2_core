3882628fe314643e74632cdbf0b8578f6fc27777
Clément Dussieux <clement.dussieux@pollen-robotics.com>
2024-03-12 15:53:06 +0100

    some commit

--- reachy_utils/reachy_utils/launch.py
+++ reachy_utils/reachy_utils/launch.py
@@ -1,21 +1,21 @@
 import os
-import threading    
-from rclpy.logging import get_logging_directory
+import threading
 
 from launch import LaunchContext, LaunchDescription
 from launch.actions import (
     DeclareLaunchArgument,
+    ExecuteProcess,
     IncludeLaunchDescription,
     LogInfo,
     OpaqueFunction,
     RegisterEventHandler,
     TimerAction,
-    ExecuteProcess
 )
 from launch.event_handlers import OnProcessExit
 from launch.substitutions import PathJoinSubstitution
 from launch_ros.actions import Node
 from launch_ros.substitutions import FindPackageShare
+from rclpy.logging import get_logging_directory
 
 report_lock = threading.Lock()
 failed_nodes = []
@@ -24,18 +24,14 @@ success_nodes = []
 
 # Helper function to get configuration file path
 def get_fake(package: str, filename: str, context: LaunchContext) -> str:
-    return PathJoinSubstitution(
-        [FindPackageShare(package), "config", filename]
-    ).perform(context)
+    return PathJoinSubstitution([FindPackageShare(package), "config", filename]).perform(context)
 
 
 def get_rviz_conf_choices() -> list[str]:
     """Lists all .rviz files in reachy_description/config, stripping the .rviz extension."""
 
     rviz_config_choices = []
-    for file in os.listdir(
-        os.path.dirname(os.path.realpath(__file__)) + "/../../reachy_description/config"
-    ):
+    for file in os.listdir(os.path.dirname(os.path.realpath(__file__)) + "/../../reachy_description/config"):
         if file.endswith(".rviz"):
             rviz_config_choices.append(file[:-5])
     return rviz_config_choices
@@ -58,9 +54,7 @@ def parseTacus(tacus, context):
 
     elif isinstance(tacus, (IncludeLaunchDescription, LaunchDescription)):
         # launchDescription does not have a condition attribute
-        if isinstance(tacus, LaunchDescription) or (
-            tacus.condition is not None and tacus.condition.evaluate(context=context)
-        ):
+        if isinstance(tacus, LaunchDescription) or (tacus.condition is not None and tacus.condition.evaluate(context=context)):
             browse_sub_tacus(tacus.visit(context=context))
 
     elif isinstance(tacus, RegisterEventHandler):
@@ -84,9 +78,7 @@ def title_print(title: str) -> LogInfo:
 
 
 def get_node_list(nodes, context: LaunchContext):
-    packed_node_list = [
-        parseTacus(node, context) for node in nodes
-    ]  # "Unpack not allowed in comprehension" :facepalm:
+    packed_node_list = [parseTacus(node, context) for node in nodes]  # "Unpack not allowed in comprehension" :facepalm:
     # print(packed_node_list)
     flattened_list = [node for sublist in packed_node_list for node in sublist]
     return flattened_list
@@ -110,7 +102,7 @@ def watcher_report(nb_node: int, delay: float = 5.0) -> TimerAction:
     def print_report(context):
         reporting_nodes = len(failed_nodes) + len(success_nodes)
         unmonitored_nodes = nb_node - reporting_nodes
-        report_message = f"Node Watcher Report\n Logging to {get_current_run_log_dir()}\n Un-monitored nodes[{unmonitored_nodes}/{nb_node}]\n Success[{len(success_nodes)}/{reporting_nodes}]: {success_nodes}\n Failed[{len(failed_nodes)}/{reporting_nodes}]: {failed_nodes}"
+        report_message = f"Node Watcher Report\n Logging to {get_current_run_log_dir()}\n Un-monitored nodes[{unmonitored_nodes}/{nb_node}]\n Failed[{len(failed_nodes)}/{reporting_nodes}]: {failed_nodes}\n Success[{len(success_nodes)}/{reporting_nodes}]: {success_nodes}"
         return [title_print(report_message)]
 
     return TimerAction(
@@ -130,9 +122,7 @@ def build_watchers_from_node_list(node_list: list[Node]) -> list[RegisterEventHa
                 event_handler=OnProcessExit(
                     target_action=node,
                     on_exit=[
-                        OpaqueFunction(
-                            function=lambda context: check_node_status(context)
-                        ),
+                        OpaqueFunction(function=lambda context: check_node_status(context)),
                     ],
                 )
             )
@@ -143,9 +133,8 @@ def build_watchers_from_node_list(node_list: list[Node]) -> list[RegisterEventHa
     return watchmen
 
 
+def get_current_run_log_dir() -> str:
 
-def get_current_run_log_dir()->str:
-    
     log_dir = get_logging_directory()
 
     # List all items in the log directory
