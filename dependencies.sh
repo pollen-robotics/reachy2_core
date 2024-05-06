642778b2c8ff84f06c38182138da00816e33bcc5
cdussieux <clementdussieux@hotmail.fr>
2024-05-06 16:07:20 +0200

    Update dependencies.sh

--- dependencies.sh
+++ dependencies.sh
@@ -4,5 +4,3 @@ sudo apt-get install -y python3-pip python3-pykdl
 sudo apt-get install -y ament-cmake
 sudo apt-get install -y ros-humble-ros2-control ros-humble-joint-state-publisher-gui ros-humble-ros2-controllers ros-humble-xacro ros-humble-rmw-cyclonedds-cpp ros-humble-gazebo-ros ros-humble-gazebo-ros-pkgs ros-humble-gazebo-ros2-control  ros-humble-tf-transformations ros-humble-hardware-interface ros-humble-controller-interface ros-humble-realtime-tools ros-humble-vision-msgs ros-humble-rviz2 ros-humble-compressed-image-transport ros-humble-foxglove-bridge
 sudo apt-get install -y libclang-dev libudev-dev pkg-config
-#cargo install cargo-ament-build
-cargo install --git https://github.com/jerry73204/cargo-ament-build.git --branch conditionally-copy-cargo-lock-file
